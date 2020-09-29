2edab8f4583d4b8657209de3f9aaa355620738b4
rzzzwilson <rzzzwilson@gmail.com>
2020-09-29 15:23:22 +0700

    Added tkinter version

--- README.rst
+++ README.rst
@@ -10,8 +10,9 @@ initialise the GUI environment.  You can't raise any sort of dialog to
 warn the user and you can't **print** anything as there is no terminal to
 show the error text.
 
-My first attempt to solve this problem used the Tkinter module.  This is
-part of python, so should always be there, right?  Ha!  Under Ubuntu the
+My first attempt to solve this problem
+`used the Tkinter module <https://github.com/rzzzwilson/notify/tkinter_notify.py>`_.
+This is part of python, so should always be there, right?  Ha!  Under Ubuntu the
 Tkinter module is not installed automatically, you have to do:
 
 ::
---
+++ tkinter_notify.py
@@ -0,0 +1,19 @@
+"""
+Tkinter solution to displaying text on error in GUI program.
+
+Usage: notify(title='Test', message='Test message')
+"""
+
+try:
+    from Tkinter import Tk
+    import tkMessageBox as messagebox
+except ImportError:
+    from tkinter import Tk, messagebox
+
+def notify(title='', message=''):
+    Tk().withdraw()
+    messagebox.showinfo(title=title, message=message) 
+
+if __name__ == '__main__':
+    msg = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    notify('Lorem Ipsum', msg)
